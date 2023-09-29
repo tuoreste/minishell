88d194cc5e3159814d43f4ef6d045fd2d2aae751
akramguediri <akramguediri2@gmail.com>
2023-09-29 18:18:00 +0200

    added all the libraries required

--- minishell.h
+++ minishell.h
@@ -6,14 +6,24 @@
 /*   By: aguediri <aguediri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 16:52:10 by aguediri          #+#    #+#             */
-/*   Updated: 2023/09/29 17:12:06 by aguediri         ###   ########.fr       */
+/*   Updated: 2023/09/29 18:17:22 by aguediri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
-# include <stdio.h>
-# include <sys/errno.h>
-# include <unistd.h>
+/*c libraries*/
+#include <readline/readline.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <dirent.h>
+#include <string.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+/*other*/
 # include "parser/parser.h"
 # include "executer/executer.h"
 
