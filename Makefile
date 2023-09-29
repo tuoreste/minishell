f7c223c5dd0a390932417867a8fe589c7b6927a0
akramguediri <akramguediri2@gmail.com>
2023-09-29 17:13:12 +0200

    added makefile & *h

---
+++ Makefile
@@ -0,0 +1,33 @@
+# NAMEutable name
+NAME = minishell
+
+# Compiler and flags
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror
+
+# Source files
+SRCS = #main.c shell.c commands.c
+
+# Object files
+OBJS = $(SRCS:.c=.o)
+
+# Default target
+all: $(NAME)
+
+# Compile the NAMEutable
+$(NAME): $(OBJS)
+	$(CC) $(CFLAGS) -o $@ $(OBJS)
+
+# Compile individual source files
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+# Clean up object files and the NAMEutable
+fclean:
+	rm -f $(OBJS) $(NAME)
+
+clean:
+	rm -f $(OBJS)
+
+# Phony targets
+.PHONY: all fclean clean 
--- minishell.h
+++ minishell.h
@@ -6,15 +6,15 @@
 /*   By: aguediri <aguediri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 16:52:10 by aguediri          #+#    #+#             */
-/*   Updated: 2023/09/29 16:54:47 by aguediri         ###   ########.fr       */
+/*   Updated: 2023/09/29 17:12:06 by aguediri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
-#include <unistd.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/errno.h>
-
+# include <stdio.h>
+# include <sys/errno.h>
+# include <unistd.h>
+# include "parser/parser.h"
+# include "executer/executer.h"
 
 #endif
