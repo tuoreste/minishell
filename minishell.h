cbd71aa13711cc005ba012bf8f309429ea017bdc
akramguediri <akramguediri2@gmail.com>
2023-09-29 16:55:21 +0200

    added libraries

--- minishell.h
+++ minishell.h
@@ -6,7 +6,15 @@
 /*   By: aguediri <aguediri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 16:52:10 by aguediri          #+#    #+#             */
-/*   Updated: 2023/09/29 16:52:11 by aguediri         ###   ########.fr       */
+/*   Updated: 2023/09/29 16:54:47 by aguediri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MINISHELL_H
+#include <unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/errno.h>
+
+
+#endif
