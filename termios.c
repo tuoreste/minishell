360a56e294b7a6555efcb58eb89d04c59df0a319
Oreste <tuoreste@gmail.com>
2023-10-06 13:37:48 +0200

    termios working 70%

--- termios.c
+++ termios.c
@@ -6,7 +6,7 @@
 /*   By: otuyishi <otuyishi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 12:47:52 by otuyishi          #+#    #+#             */
-/*   Updated: 2023/10/03 19:24:42 by otuyishi         ###   ########.fr       */
+/*   Updated: 2023/10/06 13:28:24 by otuyishi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,12 +69,65 @@ char	*read_command(void)
 	return (input);
 }
 
+void	custom_clear(void)
+{
+	if (isatty(STDOUT_FILENO))
+	{
+		const char *clear_sequence = "\x1b[H\x1b[2J";
+		write(STDOUT_FILENO, clear_sequence, strlen(clear_sequence));
+	}
+}
+
+char	*find_command_in_path(const char *command_name)
+{
+	char	*path;
+	char	*path_copy;
+	char	*token;
+	char	*full_path;
+
+	path = getenv("PATH");
+	if (path == NULL)
+	{
+		return (NULL);
+	}
+	path_copy = strdup(path);
+	if (path_copy == NULL)
+	{
+		perror("strdup");
+		exit(EXIT_FAILURE);
+	}
+	token = strtok(path_copy, ":");
+	while (token != NULL)
+	{
+		full_path = malloc(strlen(token) + strlen(command_name) + 2);
+		if (full_path == NULL)
+		{
+			perror("malloc");
+			exit(EXIT_FAILURE);
+		}
+		sprintf(full_path, "%s/%s", token, command_name);
+		if (access(full_path, X_OK) == 0)
+		{
+			free(path_copy);
+			return (full_path);
+		}
+		free(full_path);
+		token = strtok(NULL, ":");
+	}
+	free(path_copy);
+	return (NULL);
+}
+
 void	execute_command(char *command)
 {
 	int		fd[2];
 	pid_t	child_pid;
 	char	buffer[4096];
 	ssize_t	read_bytes;
+	char	*token;
+	int		arg_count;
+	char	*full_path;
+	char	**args;
 
 	if (pipe(fd) == -1)
 	{
@@ -92,8 +145,38 @@ void	execute_command(char *command)
 		close(fd[0]);
 		dup2(fd[1], STDOUT_FILENO);
 		close(fd[1]);
-		execlp("/bin/sh", "/bin/sh", "-c", command, NULL);
-		perror("execlp");
+		token = strtok(command, " ");
+		args = NULL;
+		arg_count = 0;
+		while (token != NULL)
+		{
+			args = realloc(args, sizeof(char *) * (arg_count + 1));
+			if (args == NULL)
+			{
+				perror("realloc");
+				exit(EXIT_FAILURE);
+			}
+			args[arg_count] = token;
+			arg_count++;
+			token = strtok(NULL, " ");
+		}
+		args = realloc(args, sizeof(char *) * (arg_count + 1));
+		if (args == NULL)
+		{
+			perror("realloc");
+			exit(EXIT_FAILURE);
+		}
+		args[arg_count] = NULL;
+		full_path = find_command_in_path(args[0]);
+		if (full_path == NULL)
+		{
+			fprintf(stderr, "Command not found: %s\n", args[0]);
+			exit(EXIT_FAILURE);
+		}
+		execve(full_path, args, NULL);
+		perror("execve");
+		free(args);
+		free(full_path);
 		exit(EXIT_FAILURE);
 	}
 	else
@@ -106,7 +189,7 @@ void	execute_command(char *command)
 	}
 }
 
-int	termio(void)
+int	main(int argc, char **argv, char **env)
 {
 	struct termios	saved_attributes;
 	char			*command;
@@ -115,13 +198,15 @@ int	termio(void)
 	init_termios(&saved_attributes);
 	while (1)
 	{
-		//printf("Welcome to my minishell!\n");
+		// printf("Welcome to my minishell!\n");
 		command = read_command();
 		if (ft_memcmp(command, "exit", 4) == 0)
 		{
 			free(command);
 			break ;
 		}
+		else if (strcmp(command, "clear") == 0)
+			custom_clear();
 		execute_command(command);
 		free(command);
 	}
